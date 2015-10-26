c03b6df011eecee5347831387779b6edea8f5dce
oraisdy <569450503@qq.com>
2015-10-26 10:16:33 +0800

    po小改

---
+++ ExpressSystem/src/client/po/strategypo/SalaryPO.java
@@ -0,0 +1,42 @@
+package client.po.strategypo;
+
+import java.io.Serializable;
+
+public class SalaryPO implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9132465046645093029L;
+
+	private double clerkSalary;
+	
+	private double driverWage;
+	
+	private double postmanWage;
+
+	public double getClerkSalary() {
+		return clerkSalary;
+	}
+
+	public double getDriverWage() {
+		return driverWage;
+	}
+
+	public double getPostmanWage() {
+		return postmanWage;
+	}
+
+	public void setClerkSalary(double clerkSalary) {
+		this.clerkSalary = clerkSalary;
+	}
+
+	public void setDriverWage(double driverWage) {
+		this.driverWage = driverWage;
+	}
+
+	public void setPostmanWage(double postmanWage) {
+		this.postmanWage = postmanWage;
+	}
+	
+	
+}
