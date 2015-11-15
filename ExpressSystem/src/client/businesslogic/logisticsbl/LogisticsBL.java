89c8ef4d4de75d9e822e812c29af65f63a70566a
Camilla-NJU <guhan.1996@qq.com>
2015-11-15 13:09:04 +0800

    o，忘了保存了

--- ExpressSystem/src/client/businesslogic/logisticsbl/LogisticsBL.java
+++ ExpressSystem/src/client/businesslogic/logisticsbl/LogisticsBL.java
@@ -1,5 +1,14 @@
 package client.businesslogic.logisticsbl;
 
+import client.blservice.logisticsblservice.LogisticsBLService;
+import client.vo.logisticsvo.LogisticsVO;
+
 public class LogisticsBL implements LogisticsBLService{
+
+	@Override
+	public LogisticsVO view(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
