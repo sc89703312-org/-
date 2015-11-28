781b6409c612381fe84260d4c2c7bd92c99133f6
sc89703312 <89703312@qq.com>
2015-11-28 12:07:17 +0800

    test

--- express-demo-master/client/src/main/java/edu/nju/express/init/client.java
+++ express-demo-master/client/src/main/java/edu/nju/express/init/client.java
@@ -1,11 +1,16 @@
 package edu.nju.express.init;
 
 
+
+
 public class client {
 
 	public static void main(String[] s){
 		RMIHelper.init();
 		System.out.println("Client creates!");
+		
+		
+		
 		
 		}
 }
