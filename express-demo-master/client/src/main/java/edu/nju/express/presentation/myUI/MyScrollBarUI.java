6f4d3679c08c69c7082e487ab408ebe168e0af83
oraisdy <569450503@qq.com>
2015-12-02 20:27:28 +0800

    .

--- express-demo-master/client/src/main/java/edu/nju/express/presentation/myUI/MyScrollBarUI.java
+++ express-demo-master/client/src/main/java/edu/nju/express/presentation/myUI/MyScrollBarUI.java
@@ -1,12 +1,7 @@
 package edu.nju.express.presentation.myUI;
 
-import static sun.swing.SwingUtilities2.drawHLine;
-import static sun.swing.SwingUtilities2.drawRect;
-import static sun.swing.SwingUtilities2.drawVLine;
-
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
@@ -17,15 +12,42 @@ import javax.swing.plaf.basic.BasicScrollBarUI;
 
 public class MyScrollBarUI extends BasicScrollBarUI {
 
+	private int width = 11;
+	private Color thumbColor = new Color(44, 62, 80);
+	private Color trackColor = new Color(238, 238, 238);
+	
+	public MyScrollBarUI() {
+		super();
+	}
+	
+	public MyScrollBarUI(int width){
+		super();
+		this.width = width;
+	}
+	
+	public void setThumbColor(Color thumbColor){
+		this.thumbColor = thumbColor;
+	}
+	
+	public void setTrackColor(Color trackColor){
+		this.trackColor = trackColor;
+	}
+	
+	public void setWidth(int width){
+		this.width = width;
+	}
+	
 	@Override
+	//滚动条宽度
 	public Dimension getPreferredSize(JComponent c) {
-		// TODO Auto-generated method stub
 		return new Dimension(11, 11);
 	}
+	
+	
 
 	@Override
+	//绘制滑块
 	protected void paintThumb(Graphics g, JComponent c, Rectangle thumbBounds) {
-		// TODO Auto-generated method stub
 		if (thumbBounds.isEmpty() || !scrollbar.isEnabled()) {
 			return;
 		}
@@ -35,48 +57,35 @@ public class MyScrollBarUI extends BasicScrollBarUI {
 
 		g.translate(thumbBounds.x, thumbBounds.y);
 
-		g.setColor(new Color(44, 62, 80));
+		g.setColor(thumbColor);
 		g.fillRoundRect(0, 0, w - 1, h - 1, 8, 8);
 		g.drawRoundRect(0, 0, w - 1, h - 1, 8, 8);
-
-		/*
-		 * g.setColor(thumbDarkShadowColor); drawRect(g, 0, 0, w - 1, h - 1);
-		 * g.setColor(thumbColor); g.fillRect(0, 0, w - 1, h - 1);
-		 * 
-		 * g.setColor(thumbHighlightColor); drawVLine(g, 1, 1, h - 2);
-		 * drawHLine(g, 2, w - 3, 1);
-		 * 
-		 * g.setColor(thumbLightShadowColor); drawHLine(g, 2, w - 2, h - 2);
-		 * drawVLine(g, w - 2, 1, h - 3);
-		 */
 
 		g.translate(-thumbBounds.x, -thumbBounds.y);
 	}
 
 	@Override
+	//绘制轨道
 	protected void paintTrack(Graphics g, JComponent c, Rectangle trackBounds) {
 
-		g.setColor(new Color(238, 238, 238));
-		g.fillRect(trackBounds.x, trackBounds.y, trackBounds.width, trackBounds.height);
-
-		/*
-		 * if(trackHighlight == DECREASE_HIGHLIGHT) { paintDecreaseHighlight(g);
-		 * } else if(trackHighlight == INCREASE_HIGHLIGHT) {
-		 * paintIncreaseHighlight(g); }
-		 */
+		g.setColor(trackColor);
+		g.fillRoundRect(trackBounds.x, trackBounds.y, trackBounds.width-1, trackBounds.height-1,8,8);
 	}
 
 	@Override
+	//绘制上三角
 	protected JButton createIncreaseButton(int orientation) {
-		// TODO Auto-generated method stub
 		return new MyArrowButton(orientation);
 	}
 
 	@Override
+	//绘制下三角
 	protected JButton createDecreaseButton(int orientation) {
 		return new MyArrowButton(orientation);
 	}
-
+	
+	
+	//三角箭头
 	private class MyArrowButton extends BasicArrowButton {
 
 		public MyArrowButton(int orientation) {
@@ -91,7 +100,6 @@ public class MyScrollBarUI extends BasicScrollBarUI {
 
 		@Override
 		public Dimension getPreferredSize() {
-			// TODO Auto-generated method stub
 			return new Dimension(0, 0);
 		}
 		
