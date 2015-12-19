1db9bbf3c2dcee7bc42665b7a8457a91d9158d7b
sc89703312 <89703312@qq.com>
2015-12-20 00:36:06 +0800

    期初建账

--- express-demo-master/server/src/main/java/edu/nju/express/data/accountdata/AccountDataTest.java
+++ express-demo-master/server/src/main/java/edu/nju/express/data/accountdata/AccountDataTest.java
@@ -3,6 +3,7 @@ package edu.nju.express.data.accountdata;
 
 
 import edu.nju.express.common.CommonData;
+import edu.nju.express.common.ResultMessage;
 import edu.nju.express.po.Accountpo;
 
 public class AccountDataTest extends CommonData<Accountpo> implements AccountDao {
@@ -20,6 +21,18 @@ public class AccountDataTest extends CommonData<Accountpo> implements AccountDao
 	private static final long serialVersionUID = 1L;
 	
 	
+	@Override
+	public ResultMessage insert(Accountpo e) {
+		// TODO Auto-generated method stub
+		
+		if(inList.insert(e)==ResultMessage.INVALID)
+		{
+			inList.update(e.getId(), e);
+		}
+		
+		
+		return ResultMessage.VALID;
+	}
 	
 	
 	
